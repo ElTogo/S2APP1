fca340d6a0e05fd2f1f8617fc8244e063cef34ac
MathisViegas <91677579+MathisViegas@users.noreply.github.com>
2022-01-17 22:41:55 -0500

    Add files via upload

--- vecteur.cpp
+++ vecteur.cpp
@@ -17,9 +17,7 @@ Vecteur::Vecteur(int _capacite)
 
 Vecteur::~Vecteur()
 {
-	cout<<"destructeur vecteur"<<endl;
-	//this->viderVecteur();
-	cout<<"destructeur vecteur terminer"<<endl;
+	
 }
 	
 bool Vecteur::push_back(Forme* forme)
@@ -65,13 +63,13 @@ bool Vecteur::vecteurVide()
 
 void Vecteur::viderVecteur()
 {
-	cout<<"viderVecteur"<<endl;
+	
 	for (int i = 0; i < taille; i++)
 	{
 		delete tableau[i];
 	}
 	taille=0;
-	cout<<"viderVecteur termine"<<endl;
+	
 }
 
 int Vecteur::getTaille()
